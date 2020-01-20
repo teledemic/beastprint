e3c3cc48b7bfd31c800ae8a45cf7cfa9247c60e4
Carl Irving <carl@teledemic.com>
2020-01-20 13:01:26 -0500

    Hammer of Gond

--- src/components/MagicItem.vue
+++ src/components/MagicItem.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="item-card">
-    <div class="item-stats">
+    <!-- <div class="item-stats">
       <div class="item-name">{{item.name}}</div>
       <div class="item-type">{{item.type}}, {{item.rarity}}</div>
       <div class="description">{{item.description}}</div>
       <div class="description">&nbsp;&nbsp;&nbsp;{{item.description2}}</div>
+    </div> -->
+    <div class="item-stats">
+      <div class="item-name">Hammer of Gond</div>
+      <div class="item-type">Weapon (warhammer), legendary</div>
+      <div class="item-type">Wielding this hammer requires a strength score of 16 or higher.</div>
+      <div class="description">You gain a +1 bonus to attack and damage rolls made with this magic weapon.</div>
+      <div class="description"><b>Forge's Rhythm:</b></div>
+      <div class="description">&nbsp;&nbsp;&nbsp;You may spend an action hefting the hammer and beginning to swing it about your body.</div>
+      <div class="description">&nbsp;&nbsp;&nbsp;Your movement speed is halved while spinning the hammer, and you may not take reactions. If you become prone, restrained, or incapacitated, you stop spinning.</div>
+      <div class="description">&nbsp;&nbsp;&nbsp;On your next turn, if you are still spinning, any hits with the hammer do triple damage. You stop spinning at the end of the turn.</div>
+      <div class="description"><b>Fury of Gond (Requires Attunement by a cleric or paladin):</b></div>
+      <div class="description">&nbsp;&nbsp;&nbsp;As an action, you may smash this hammer into the ground to cast the Thunderwave spell as a 4th level spell, DC15. The hammer can't be used this way again until the next dawn.</div>
+      <div class="description">&nbsp;&nbsp;&nbsp;<i>All creatures besides you in a 15-foot cube make DC15 CON save. On fail, 5d8 thunder damage and pushed 10 feet away. On save, 1/2 damage.</i></div>
     </div>
     <div class="item-pic">
-      <img src="../assets/talon.jpg" />
+      <img src="../assets/warhammer.png" />
     </div>
   </div>
 </template>
